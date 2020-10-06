0ed8bddd82c67819e87654999dded769548fda93
JenisVaghasiya <72142142+JenisVaghasiya@users.noreply.github.com>
2020-10-06 18:42:41 +0530

    Update models.py

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Contact(models.Model):
     name = models.CharField(max_length=120)
     email = models.CharField(max_length=120)
-    phone = models.CharField(max_length=12)
+    phone = models.CharField(max_length=100)
     desc = models.TextField()
     date = models.DateField()
 
